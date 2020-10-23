09ab8e4a5eac17c57ed328abf30321ca7acfdce7
Johannes Maas <y0hy0h@gmx.net>
2020-10-23 05:06:00 +0200

    Fix deployment path

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   chainWebpack: config => {
     config
-        .plugin('html')
-        .tap(args => {
-            args[0].title = "Loopbar";
-            return args;
-        })
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Loopbar'
+        return args
+      })
   },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/loopbar/'
+    : '/',
   lintOnSave: true
 }
